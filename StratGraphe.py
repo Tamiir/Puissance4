1010a08f8a999726deaf7e638d83bbb881400ed1
Tamiir <paul@roqueta.fr>
2023-08-09 17:25:24 +0200

    Added a function to play against the computer

--- StratGraphe.py
+++ StratGraphe.py
@@ -25,3 +25,36 @@ def calculer_strategie():
                 dico[etat] = (3 - etat[0], coups_prochains[3 - etat[0]])
     aux(init()) # on commence à l'état initial
     return dico
+
+def jouer(humain):
+    d = calculer_strategie()
+    etat = init()
+    display(etat)
+    while coups_possibles(etat) != [] and est_gagnant(etat) == 0:
+        joueur = etat[0]
+        grille = etat[1]
+        if joueur == humain:
+            print("À vous de jouer.")
+            while True:
+                s = input("> ")
+                if s not in ['0','1','2','3','4','5','6','7']:
+                    print("Colonne non comprise.")
+                    continue
+                x = int(s)
+                if grille[0][x] != 0:
+                    print("Colonne pleine.")
+                    continue
+                coup = x
+                break
+        else: # joueur == 2
+            coup = d[etat][1]
+            assert coup != []
+        print(f"Joueur {joueur} joue en {coup}")
+        etat = jouer_coup(etat, coup)
+        display(etat)
+    print("Fin de partie")
+    vainqueur = est_gagnant(etat)
+    if vainqueur == 0:
+        print("Match nul")
+    else:
+        print(f"Le vainqueur est {vainqueur}.")
