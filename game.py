1e9ad0acb8988266f49c32da7684d2f9957cb332
Tamiir <paul@roqueta.fr>
2023-08-09 18:02:03 +0200

    minor fix

--- game.py
+++ game.py
@@ -20,27 +20,27 @@ def est_gagnant(etat):
     #victoire en ligne
     for ligne in range(6):
         for j in range(3):
-            if grille[ligne][j] == grille[ligne][j+1] == grille[ligne][j+2] == grille[ligne][j+3] and grille[ligne][j] != 0:
+            if grille[ligne][j] == grille[ligne][j+1] == grille[ligne][j+2] == grille[ligne][j+3] and (grille[ligne][j] == 1 or grille[ligne][j] == 2):
                 #print('victoire en ligne joueur ',grille[ligne][j])
                 return grille[ligne][j]
             
     #victoire en colonne
     for colonne in range(7):
         for j in range(3):
-            if grille[j][colonne] == grille[j+1][colonne] == grille[j+2][colonne] == grille[j+3][colonne] and grille[j][colonne] != 0:
+            if grille[j][colonne] == grille[j+1][colonne] == grille[j+2][colonne] == grille[j+3][colonne] and (grille[j][colonne] == 1 or grille[j][colonne] == 2):
                 #print('victoire en colonne joueur ',grille[j][colonne])
                 return grille[j][colonne]
             
     #victoire en diagonal
     for ligne in range(3,6):
         for colonne in range(4):
-            if grille[ligne][colonne] == grille[ligne-1][colonne+1] == grille[ligne-2][colonne+2] == grille[ligne-3][colonne+3] and grille[ligne][colonne] != 0:
+            if grille[ligne][colonne] == grille[ligne-1][colonne+1] == grille[ligne-2][colonne+2] == grille[ligne-3][colonne+3] and (grille[ligne][colonne] ==1 or grille[ligne][colonne] ==2):
                 return grille[ligne][colonne]
     
     #victoire contre-diago
     for ligne in range(3):
         for colonne in range(4):
-            if grille[ligne][colonne] == grille[ligne+1][colonne+1] == grille[ligne+2][colonne+2] == grille[ligne+3][colonne+3] and grille[ligne][colonne] != 0:
+            if grille[ligne][colonne] == grille[ligne+1][colonne+1] == grille[ligne+2][colonne+2] == grille[ligne+3][colonne+3] and (grille[ligne][colonne] ==1 or grille[ligne][colonne] ==2):
                 return grille[ligne][colonne]
 
     #pas de victoire        
@@ -64,7 +64,7 @@ def jouer_coup(etat,coup):
 
 def display(etat):
     grille = etat[1]
-    remp = ['.','X','O']
+    remp = ['.','X','O','+']
     print("  0 1 2 3 4 5 6")
     for i in range(6):
         print(i, " ", sep = "", end = "")
